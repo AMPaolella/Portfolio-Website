584a53f5f21c93917fa9b160bc204dfadb65b9aa
Andrew Paolella <andrewpaolella@APspy.local>
2025-07-19 13:17:01 -0400

    update

--- js/index.js
+++ js/index.js
@@ -8,9 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
       <hr class="solid" />
       <div class="content-details">
         <p>
-        Greetings! I'm Andrew Paolella, an Information Technology professional fueled by a passion for technology. My journey with computers began practically before I could tie my shoes, sparking an innate curiosity that has driven me ever since. My broad skillset encompasses a strong foundation in programming languages including Python and C++. Beyond programming, my technical abilities extend to system operations, web design, ethical hacking, and computer hardware. I am seeking full-time, or freelance opportunities to apply my expertise. </p>
-        <p>In my freetime, I enjoy staying active with outdoor pursuits, reading new books, traveling, music, and live comedy. I'm an advocate for self-development and learning new things.
-        I possess strong problem-solving and troubleshooting abilities, allowing me to efficiently tackle challenges and optimize workflows.</p>
+        Greetings! I'm Andrew Paolella, an IT professional with a lifelong love for technology. My journey with computers began practically before I could tie my shoes, sparking an innate curiosity that has driven me ever since. My broad skillset encompasses a strong foundation in programming languages including Python and C++. Beyond programming, my technical abilities extend to system operations, embedded systems, web design, information security, and more! I am seeking full-time, or freelance opportunities to apply my expertise. </p>
+        <p>In my freetime, I enjoy staying active outdoors, reading new books, traveling, music, and comedy. I'm a strong problem-solver with a continuous drive for learning and personal development. </p>
         </p>
       </div>
     </section>
@@ -24,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
         <h3>Oakland University</h3>
         <p>Bachelor of Science in Information Technology</p>
         <p>2023 - 2025</p>
-        <p> As a graduate of Oaklands ABET accredited IT program, I gained a strong foundation in programming, HCI, edge AI, embedded systems, web and mobile development, computer networks, database design, data structures, systems administration and much more. 
+        <p> As a graduate of Oaklands ABET accredited IT program, I gained a strong foundation of fundamentals in programming, HCI, AI embedded systems, web and mobile development, computer networks, database design, data structures, systems administration and much more. 
         I tailored my studies to my specific interests within the program through specialized cybersecurity tracks including, information security, security and privacy in computing, information security practices, and script programming.
       </div>
     </section>
@@ -45,8 +44,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
         </p>
       </div>
+
+      <div class="content-details">
+        <h3>IT Support Specialist</h3>
+        <p>Weber Olcese PLC</p>
+        <p>2025 - Present</p>
+        <p>
+        At Weber Olcese I am responsible for overseaing and maintaining their IT infrastructure. My responsibilities include providing technical support and troubleshooting for end-users. Install, configure, monitor, and maintain computer systems, networks, and applications. Manage Active Directory: user and computer accounts, group policies, and security groups. 
+        Manage Office 365 Outlook Admin: user accounts, licensing, policies, and communication. Deploy and manage Duo 2FA, onboarding users for secure system authentication.
+        
+        </p>
+      </div>
+
     </section>
   `;
+
+  const projectContent = `
+  <section class="projects-section">
+    <h2>Projects</h2>
+    <hr class="solid" />
+    <div class="content-details">
+      <h3>AI Neural Network </h3>
+      <p>
+        Developed an autonomous driving object detection pipeline across diverse driving scenarios using the BDD100K dataset, YOLO model, and PyTorch.
+      </p>
+
+      <h3>PythonDataCrawler</h3>
+      <p>
+        Python-based interactive geographic data crawler and population density visualization tool using ipyleaflet and GeoPandas. Used Jupyter for interactive maps and exploratory data analysis.
+      </p>
+      <h3>ParlayGPT</h3>
+      <p>
+        iOS sports betting app leveraging OpenAI to analyze and improve parlay bet outcomes. Still in early development.
+      </p>
+    </div>
+  </section>
+`;
+
 
   // Load initial content
   contentDiv.innerHTML = aboutContent;
@@ -58,15 +92,19 @@ document.addEventListener("DOMContentLoaded", () => {
   const aboutBtn = document.createElement("button");
   const educationBtn = document.createElement("button");
   const experienceBtn = document.createElement("button");
+  const projectBtn = document.createElement("button");
 
   aboutBtn.classList.add("circle-button");
   educationBtn.classList.add("circle-button");
   experienceBtn.classList.add("circle-button");
+  projectBtn.classList.add("circle-button");
 
   // Append buttons to the container
   buttonContainer.appendChild(aboutBtn);
   buttonContainer.appendChild(educationBtn);
   buttonContainer.appendChild(experienceBtn);
+  buttonContainer.appendChild(projectBtn);
+
 
   // Add the button container to the DOM
   document.querySelector("header").appendChild(buttonContainer);
@@ -83,4 +121,9 @@ document.addEventListener("DOMContentLoaded", () => {
   experienceBtn.addEventListener("click", () => {
     contentDiv.innerHTML = experienceContent;
   });
+
+  projectBtn.addEventListener("click", () => {
+    contentDiv.innerHTML = projectContent; 
+  });
+
 });
